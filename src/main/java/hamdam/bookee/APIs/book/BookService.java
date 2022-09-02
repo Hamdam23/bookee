836332665b746a000da2c65d4e2c798c4c883d3b
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-09-02 18:19:42 +0500

    Review: book package

--- src/main/java/hamdam/bookee/APIs/book/BookService.java
+++ src/main/java/hamdam/bookee/APIs/book/BookService.java
@@ -3,12 +3,14 @@ package hamdam.bookee.APIs.book;
 import java.util.List;
 
 public interface BookService {
+    // TODO: 9/2/22 why void
     void addBook(BookDTO book);
 
     List<BookEntity> getAllBooks();
 
     BookEntity getBookById(Long id);
 
+    // TODO: 9/2/22 why void
     void updateBook(BookDTO book, Long id);
 
     void deleteBook(Long id);
--- src/main/java/hamdam/bookee/APIs/book/BookServiceImpl.java
+++ src/main/java/hamdam/bookee/APIs/book/BookServiceImpl.java
@@ -29,6 +29,7 @@ public class BookServiceImpl implements BookService {
                 -> new ResourceNotFoundException("Book", "id", id));
     }
 
+    // TODO: 9/2/22 see todos in GenreServiceImpl:updateGenre
     @Override
     public void updateBook(BookDTO book, Long id) {
         BookEntity oldBook = bookRepository.findById(id).orElseThrow(()
@@ -43,6 +44,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public void deleteBook(Long id) {
+        // TODO: 9/2/22 existsById is enough
         bookRepository.findById(id).orElseThrow(()
                 -> new ResourceNotFoundException("Book", "id", id));
 
