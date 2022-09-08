9d3064558a2c47e480509a46720a9c5e6b416976
Hamdam <xudayberganovhamdam01@gmail.com>
2022-09-08 14:39:52 +0500

    Lower: fixed sequence bug on application.yaml

--- src/main/java/hamdam/bookee/BookeeApplication.java
+++ src/main/java/hamdam/bookee/BookeeApplication.java
@@ -1,12 +1,26 @@
 package hamdam.bookee;
 
+import hamdam.bookee.APIs.auth.AuthService;
+import hamdam.bookee.APIs.auth.RegistrationRequest;
+import hamdam.bookee.APIs.role.AppRole;
+import hamdam.bookee.APIs.role.AppRoleDTO;
+import hamdam.bookee.APIs.role.AppRoleService;
+import hamdam.bookee.APIs.role.Permissions;
+import hamdam.bookee.APIs.user.AppUser;
+import hamdam.bookee.APIs.user.AppUserRoleDTO;
+import hamdam.bookee.APIs.user.AppUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
+import static hamdam.bookee.APIs.role.Permissions.*;
+
 @SpringBootApplication
 @RequiredArgsConstructor
-public class BookeeApplication {
+public class BookeeApplication  {
 
 //    implements CommandLineRunner
 //    private final AppRoleService roleService;
@@ -21,7 +35,7 @@ public class BookeeApplication {
 //    @Override
 //    public void run(String... args) {
 //        try {
-//            AppRole author = roleService.addRole(new AppRoleDTO("author", false, Set.of(Permission.values())));
+//            AppRole author = roleService.addRole(new AppRoleDTO("author", false, Set.of(Permissions.values())));
 //            AppRole admin = roleService.addRole(new AppRoleDTO("admin", false, Set.of(
 //                    GET_BOOK,
 //                    CREATE_BOOK,
@@ -48,9 +62,9 @@ public class BookeeApplication {
 //                    UPDATE_USER
 //            )));
 //
-//            AppUser hamdam = authService.addUser(new AuthUserDTO("Hamdam", "hamdam_x", "123"));
-//            AppUser farrukh = authService.addUser(new AuthUserDTO("Farrukh", "farrukh_kh", "123"));
-//            AppUser userx = authService.addUser(new AuthUserDTO("Userx", "user_x", "123"));
+//            AppUser hamdam = authService.registerUser(new RegistrationRequest("Hamdam", "hamdam_x", "123"));
+//            AppUser farrukh = authService.registerUser(new RegistrationRequest("Farrukh", "farrukh_kh", "123"));
+//            AppUser userx = authService.registerUser(new RegistrationRequest("Userx", "user_x", "123"));
 //
 //            userService.setRoleToUser(hamdam.getId(), new AppUserRoleDTO(author.getId()));
 //            userService.setRoleToUser(farrukh.getId(), new AppUserRoleDTO(admin.getId()));
