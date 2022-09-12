0e8106a849ba5be3f0d2c425f1efaaeb43288a0e
Hamdam <xudayberganovhamdam01@gmail.com>
2022-09-12 14:02:10 +0500

    Lower: added more defining permissions.

--- src/main/java/hamdam/bookee/APIs/role/Permissions.java
+++ src/main/java/hamdam/bookee/APIs/role/Permissions.java
@@ -6,6 +6,7 @@ public enum Permissions {
     GET_BOOK,
     CREATE_BOOK,
     UPDATE_BOOK,
+    //all above 3 should be replaced with MONITOR_BOOK
     DELETE_BOOK,
 
     GET_GENRE,
@@ -13,25 +14,21 @@ public enum Permissions {
     UPDATE_GENRE,
     DELETE_GENRE,
 
-    GET_ROLE,
-    CREATE_ROLE,
-    UPDATE_ROLE,
-    DELETE_ROLE,
+//    GET_ROLE,
+//    CREATE_ROLE,
+//    UPDATE_ROLE,
+//    DELETE_ROLE,
+    MONITOR_ROLE,
 
-    GET_USER,
-    CREATE_USER,
-    UPDATE_USER,
-    DELETE_USER,
+    GET_USER, //himself/herself
+    UPDATE_USER, //himself/herself
+    DELETE_USER, //himself/herself
+    MONITOR_USER, //anyone
 
     GET_ROLE_REQUEST,
-    GET_ALL_ROLE_REQUESTS_BY_ADMIN,
     CREATE_ROLE_REQUEST,
     UPDATE_ROLE_REQUEST,
     DELETE_ROLE_REQUEST,
-    DELETE_ANY_ROLE_REQUEST_BY_ADMIN,
-
-    USER,
-    ADMIN,
-    AUTHOR
-
+    MONITOR_ROLE_REQUEST,
+    //MONITOR_ROLE_STATE
 }
