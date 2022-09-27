f1745111549241f76f5f589867f5287fce42e68f
Hamdam <xudayberganovhamdam01@gmail.com>
2022-09-28 00:27:21 +0500

    Lower: changed some codes on db migration

--- src/main/resources/db/migration/V3__insert_users.sql
+++ src/main/resources/db/migration/V3__insert_users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, user_name, password, role_id, time_stamp)
-VALUES ('Hamdam Xudayberganov', 'hamdam_kh', '$2a$10$u.olISwSqjbaZCHADL0fIuw7eBijpqzvfSavgXnPcfniJTwORGNvm', 1, NOW());
+VALUES ('Hamdam Xudayberganov', 'hamdam_kh', '$2a$10$k8Ga28fN0P3ErQQ1GLdKMuHFy/bNt3yORvhDNReFi151owCu4Wupa', 1, NOW());
 INSERT INTO users (name, user_name, password, role_id, time_stamp)
-VALUES ('Farrukh Xusainov', 'farrukh_kh', '$2a$10$u.olISwSqjbaZCHADL0fIuw7eBijpqzvfSavgXnPcfniJTwORGNvm', 2, NOW());
+VALUES ('Farrukh Xusainov', 'farrukh_kh', '$2a$10$k8Ga28fN0P3ErQQ1GLdKMuHFy/bNt3yORvhDNReFi151owCu4Wupa', 2, NOW());
 INSERT INTO users (name, user_name, password, role_id, time_stamp)
-VALUES ('User Userov', 'user_kh', '$2a$10$u.olISwSqjbaZCHADL0fIuw7eBijpqzvfSavgXnPcfniJTwORGNvm', 3, NOW());
+VALUES ('User Userov', 'user_kh', '$2a$10$k8Ga28fN0P3ErQQ1GLdKMuHFy/bNt3yORvhDNReFi151owCu4Wupa', 3, NOW());
