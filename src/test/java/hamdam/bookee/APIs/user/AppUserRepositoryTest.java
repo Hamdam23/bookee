9b6b9c8a011f9ebf1de4799a8ca6e2bc310be5e5
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-10-02 13:36:08 +0500

    Fixed AppUserRepositoryTest

--- src/test/java/hamdam/bookee/APIs/user/AppUserRepositoryTest.java
+++ src/test/java/hamdam/bookee/APIs/user/AppUserRepositoryTest.java
@@ -1,10 +1,10 @@
 package hamdam.bookee.APIs.user;
 
 import hamdam.bookee.APIs.auth.RegistrationRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,6 +15,11 @@ class AppUserRepositoryTest {
 
     @Autowired
     private AppUserRepository underTest;
+
+    @BeforeEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
 
     @Test
     void canGetUserByExitingUserName() {
