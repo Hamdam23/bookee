948b6348399523a9392715f40424025d4ee42b4f
Hamdam <xudayberganovhamdam01@gmail.com>
2022-11-02 23:58:35 +0500

    Lower: implemented GetUserByTokenTest.

--- src/test/java/hamdam/bookee/tools/token/GetUserByTokenTest.java
+++ src/test/java/hamdam/bookee/tools/token/GetUserByTokenTest.java
@@ -1,38 +1,64 @@
 package hamdam.bookee.tools.token;
 
+import hamdam.bookee.APIs.user.AppUserEntity;
 import hamdam.bookee.APIs.user.AppUserRepository;
+import hamdam.bookee.tools.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.test.context.annotation.SecurityTestExecutionListeners;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ContextConfiguration(classes = GetUserByToken.class)
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SecurityTestExecutionListeners
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class GetUserByTokenTest {
 
     @Mock
     private AppUserRepository appUserRepository;
 
-    @Autowired
+    @InjectMocks
     private GetUserByToken underTest;
 
     @Test
-    void throwsExceptionWhenUserNameIsInvalid() {
+    @WithMockUser(username = "philly")
+    void getUserByRequest_throwsExceptionWhenUserNameIsInvalid() {
         //given
-//        AppUserEntity user = new AppUserEntity("Phil", "philly", "very_secret_password");
-//
-//        SecurityContext context = SecurityContextHolder.createEmptyContext();
-//        context.setAuthentication(new UsernamePasswordAuthenticationToken(user.getUserName(), null));
-//        SecurityContextHolder.setContext(context);
-//        when(appUserRepository.findAppUserByUserName(user.getUserName())).thenReturn(Optional.empty());
-//
-//        //when
-//        //then
-//        assertThatThrownBy(() -> underTest.getClass(appUserRepository))
-//                .isInstanceOf(ResourceNotFoundException.class)
-//                .hasMessageContaining("User")
-//                .hasMessageContaining("username")
-//                .hasMessageContaining(user.getUserName());
-//        verify(appUserRepository).findAppUserByUserName(user.getUserName());
+        AppUserEntity user = new AppUserEntity("Phil", "philly", "very_secret_password");
+
+        when(appUserRepository.findAppUserByUsername(user.getUsername())).thenReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> GetUserByToken.getUserByRequest(appUserRepository))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining(user.getUsername());
+        verify(appUserRepository).findAppUserByUsername(user.getUsername());
+    }
+
+    @Test
+    @WithMockUser(username = "philly")
+    void getUserByRequest_returnValidDataWhenRequestIsValid() {
+        //given
+        AppUserEntity user = new AppUserEntity("Phil", "philly", "very_secret_password");
+
+        when(appUserRepository.findAppUserByUsername(user.getUsername())).thenReturn(Optional.of(user));
+
+        //when
+        AppUserEntity actual = GetUserByToken.getUserByRequest(appUserRepository);
+
+        //then
+        assertThat(actual.getUsername()).isEqualTo(user.getUsername());
     }
 
 }
